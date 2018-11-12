dd055a06dca9a0ab47879d3e9508ac33f26879ca
David Gavilan <david@metail.com>
2018-11-12 11:47:43 +0000

    Refactor unit tests

--- html/tests.js
+++ html/tests.js
@@ -1,15 +1,3 @@
-function compareArrays(v0, v1) {
-  if (v0.length !== v1.length) {
-    return false;
-  }
-  for (var i = 0; i < v0.length; i++) {
-    if (v0[i] !== v1[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
 QUnit.test( "matrix × vector", function( assert ) {
   // row-major
   var m = [
@@ -20,9 +8,10 @@ QUnit.test( "matrix × vector", function( assert ) {
   ];
   var v = [1, 2, 3, 4];
   var mv = MATH.mulVector(m, v);
-  assert.ok( compareArrays([6, 3, 3, 19], mv), "Passed!" );
+  // https://api.qunitjs.com/assert/deepEqual
+  assert.deepEqual(mv, [6, 3, 3, 19]);
   var mvt = MATH.mulVector(MATH.transpose(m), v);
-  assert.ok( compareArrays([-3, 5, 14, 12], mvt), "Passed!" );
+  assert.deepEqual(mvt, [-3, 5, 14, 12]);
 });
 
 QUnit.test( "matrix × matrix", function( assert ) {
@@ -35,9 +24,9 @@ QUnit.test( "matrix × matrix", function( assert ) {
   ];
   var mm = MATH.mulMatrix(m, m);
   var expected = [1, 0, 0, -4, 1, 0, 0, 2, 3, 1, 1, 8, 0, 0, 0, 9];
-  assert.ok( compareArrays(expected, mm), "Passed!" );
+  assert.deepEqual(mm, expected);
   var mt = MATH.transpose(m);
   var mmt = MATH.mulMatrix(mt, mt);
   expected = MATH.transpose(expected);
-  assert.ok( compareArrays(expected, mmt), "Passed!" );
+  assert.deepEqual(mmt, expected);
 });
