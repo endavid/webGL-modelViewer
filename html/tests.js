ee683a4941f754f726f0b787b45e0189fdb622c3
David Gavilan <david@metail.com>
2018-11-21 14:45:44 +0000

    Add support for adding poses -rotations only- and updating rotation order

--- html/tests.js
+++ html/tests.js
@@ -77,3 +77,39 @@ QUnit.test("read Collada anim", function(assert) {
     }
   });
 });
+
+QUnit.test("add pose keyframe", function(assert) {
+  var done = assert.async();
+  $.ajax({
+    async: true,
+    url: "resources/monigote.dae",
+    dataType: 'text',
+    success: function(data) {
+      var model = ColladaUtils.parseCollada(data);
+      var skinnedModel = new SkinnedModel(model.skin, model.skeleton, model.anims);
+      assert.equal(skinnedModel.keyframeCount, 5);
+      var posefile = {
+        pose: {
+          Chest: [0, -0.03, -12.15],
+          Knee_L: [-13.5, -12.28, -1.45 ],
+          Head: [0, 2, 10, 1, 1, 1, 0, -0.2, 0]
+        }
+      };
+      skinnedModel.addPose(posefile.pose);
+      assert.equal(skinnedModel.keyframeCount, 6);
+      var jro = {
+        Chest: "zxy"
+      };
+      skinnedModel.updateJointRotationOrder(jro);
+      skinnedModel.applyPose(5);
+      var expectedJM = [
+        1, 0, 0, 0,
+        0, -0.05546402, -0.9984606, 0,
+        0, 0.9984607, -0.05546402, 0,
+        0, 0.6807694000000009, 4.822213, 1
+      ];
+      assert.deepEqual(skinnedModel.joints.splice(0, 16), expectedJM);
+      done();
+    }
+  });
+});
