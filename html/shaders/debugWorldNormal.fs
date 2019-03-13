6680ff54e69ed79efc4ee6077b674e35bafc8007
David Gavilan <david@metail.com>
2019-03-13 18:14:04 +0000

    Debug world normals normalized between 0 and 1.

--- html/shaders/debugWorldNormal.fs
+++ html/shaders/debugWorldNormal.fs
@@ -6,5 +6,5 @@ varying vec3 vNormal;
 varying vec3 worldPosition;
 varying vec3 cameraPosition;
 void main(void) {
-  gl_FragColor = vec4(vNormal, 1.0);
+  gl_FragColor = vec4(0.5 * vNormal + 0.5, 1.0);
 }
