4d847a135b4b55adc9b08b2ffb4068aa726aef3e
David Gavilan <david.gavilan.ruiz@gmail.com>
2022-01-20 21:35:32 +0000

    Thicker lines

--- html/shaders/debugSolidWire.fs
+++ html/shaders/debugSolidWire.fs
@@ -8,9 +8,10 @@ varying vec3 worldPosition;
 varying vec3 cameraPosition;
 varying vec4 vertexColor;
 void main(void) {
-  vec4 lines = 1.0 / clamp(255.0 * vertexColor, 0.01, 255.0);
+  vec4 lines = 1.0 / clamp(64.0 * vertexColor, 0.0001, 64.0);
   float line = max(lines.r, max(lines.g, lines.b));
   gl_FragColor = vec4(line, line, line, 1.0);
+  //gl_FragColor.rgb = lines.rgb;
   //gl_FragColor = vec4(vertexColor.aaa, 1.0);
   //gl_FragColor = vec4(vertexColor.rgb, 1.0);
 }
